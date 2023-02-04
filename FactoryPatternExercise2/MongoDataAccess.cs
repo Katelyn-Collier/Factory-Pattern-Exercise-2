c91c999689bd7567caae9c6626ccceab51e0c661
Katelyn-Collier <katelyncollier14@gmail.com>
2023-02-04 16:26:52 -0600

    initial commit

---
+++ FactoryPatternExercise2/MongoDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class MongoDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+           new Product (){Name = "Nintendo Switch", Price = 300},
+           new Product (){Name = "Xbox Series S", Price = 250},
+           new Product (){Name = "PS5", Price = 500},
+        };
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading data from a Mongo Data Access.");
+
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data from a Mongo Data Access");
+        }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Which database would you like to work with?");
+            Console.WriteLine("> sql");
+            Console.WriteLine("> mongo");
+            Console.WriteLine("> list");
+
+            var userInput = Console.ReadLine();
+
+            IDataAccess db = DataAccessFactory.GetDataAccessType(userInput);
+
+            var products = db.LoadData();
+            db.SaveData();
+
+            foreach(var product in products)
+            {
+                Console.WriteLine($"Name: {product.Name} -- Price: {product.Price}");
+            }
         }
     }
 }
